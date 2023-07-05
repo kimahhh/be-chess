72f590c600d2c2074f1d824692983eb1dc92fdfa
kimahhh <kimahyunn132@gmail.com>
2023-07-04 17:30:37 -0700

    [ADD] Pawn representation 속성 추가

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -3,18 +3,26 @@ package softeer2nd.chess.pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final String WHITE_REPRESENTATION = "p";
+    public static final String BLACK_REPRESENTATION = "P";
 
     private String color;
+    private String representation;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, String representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return this.color;
+    }
+    public String getRepresentation() {
+        return this.representation;
     }
 }
