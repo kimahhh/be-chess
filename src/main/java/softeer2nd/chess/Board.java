18f17935076bde8e272301af61050a6f571e8129
kimahhh <kimahyunn132@gmail.com>
2023-07-04 17:48:15 -0700

    [ADD] Board 초기화, 출력 메소드 추가

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -5,9 +5,11 @@ import softeer2nd.chess.pieces.Pawn;
 import java.util.ArrayList;
 
 public class Board {
+    private ArrayList<ArrayList<Object>> board;
     private ArrayList<Pawn> Pawns;
 
     public Board() {
+        board = new ArrayList<>();
         Pawns = new ArrayList<>();
     }
 
@@ -21,5 +23,48 @@ public class Board {
 
     public Pawn findPawn(int index) {
         return Pawns.get(index);
+    }
+
+    public void initialize() {
+        Pawns.clear();
+        ArrayList<Object> whitePawns = new ArrayList<>();
+        ArrayList<Object> blackPawns = new ArrayList<>();
+        for (int i = 0;i < 8;i++) {
+            Pawn white = new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+            Pawn black = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+            Pawns.add(white);
+            whitePawns.add(white);
+            Pawns.add(black);
+            blackPawns.add(black);
+        }
+
+        board.clear();
+        ArrayList<Object> objects = new ArrayList<>();
+        for (int i = 0;i < 8;i++) {
+            objects.add(".");
+        }
+        for (int i = 0;i < 6;i++) {
+            board.add(objects);
+        }
+
+        board.add(1, blackPawns);
+        board.add(6, whitePawns);
+    }
+
+    public String print() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (ArrayList<Object> line: board) {
+            for (Object obj : line) {
+                if (obj instanceof Pawn) {
+                    Pawn pawn = (Pawn) obj;
+                    stringBuilder.append(pawn.getRepresentation());
+                }
+                else {
+                    stringBuilder.append(obj);
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
