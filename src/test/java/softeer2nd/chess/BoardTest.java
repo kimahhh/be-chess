58918720bee7689a310895f3550100428e8db722
kimahhh <kimahyunn132@gmail.com>
2023-07-05 13:34:23 +0900

    [TEST] Board 초기화 검증

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,10 +1,11 @@
 package softeer2nd.chess;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import softeer2nd.chess.pieces.Piece;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static softeer2nd.utils.StringUtils.appendNewLine;
 
 class BoardTest {
 
@@ -16,29 +17,18 @@ class BoardTest {
     }
 
     @Test
+    @DisplayName("Board 초기화 검증")
     public void create() {
-        verifyBoard(Piece.createWhitePawn(), 1, 0);
-        verifyBoard(Piece.createBlackPawn(), 2, 1);
-    }
-
-    private void verifyBoard(Piece piece, int size, int index) {
-        board.add(piece);
-        assertEquals(size, board.size());
-        assertEquals(piece, board.findPawn(index));
-    }
-
-    @Test
-    public void boardPrint() {
-        initialize();
-        String strBoard = "........\nPPPPPPPP\n........\n........\n" +
-                          "........\n........\npppppppp\n........\n";
-        assertEquals(strBoard, board.print());
-    }
-
-    private void initialize() {
         board.initialize();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+        assertEquals(32, board.pieceCount());
+        String blackRank = appendNewLine("........");
+        assertEquals(
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blackRank + blackRank + blackRank + blackRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"),
+                board.showBoard()
+        );
     }
-
 }
