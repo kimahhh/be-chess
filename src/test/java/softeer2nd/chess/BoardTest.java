3928ca78610287549b43b2dce5b1eb3f1860fa62
kimahhh <kimahyunn132@gmail.com>
2023-07-06 13:49:31 +0900

    Test: Board 초기화, 기물 개수 반환 검증

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static softeer2nd.utils.StringUtils.appendNewLine;
+import static softeer2nd.chess.pieces.Piece.*;
 
 class BoardTest {
 
     private Board board;
+    private String sample = ".KR....." +
+                            "P.PB...." +
+                            ".P..Q..." +
+                            "........" +
+                            ".....nq." +
+                            ".....p.." +
+                            "......p." +
+                            "....rk..";
 
     @BeforeEach
     public void setup() {
@@ -17,10 +26,9 @@ class BoardTest {
     }
 
     @Test
-    @DisplayName("Board 초기화 검증")
-    public void create() {
+    @DisplayName("Board를 초기화할 수 있어야 한다")
+    public void initialize() {
         board.initialize();
-        assertEquals(32, board.pieceCount());
         String blackRank = appendNewLine("........");
         assertEquals(
                 appendNewLine("RNBQKBNR") +
@@ -30,5 +38,39 @@ class BoardTest {
                         appendNewLine("rnbqkbnr"),
                 board.showBoard()
         );
+    }
+
+    @Test
+    @DisplayName("Board의 특정한 상태로 초기화할 수 있어야 한다")
+    public void initializeState() {
+        String boardString = ".KR.....\n" +
+                "P.PB....\n" +
+                ".P..Q...\n" +
+                "........\n" +
+                ".....nq.\n" +
+                ".....p..\n" +
+                "......p.\n" +
+                "....rk..\n";
+        board.initialize(sample);
+        assertEquals(boardString, board.showBoard());
+    }
+
+    @Test
+    @DisplayName("모든 기물의 개수를 반환할 수 있어야 한다")
+    public void getPieceCount() {
+        board.initialize();
+        assertEquals(32, board.pieceCount());
+
+        board.initialize(sample);
+        assertEquals(13, board.pieceCount());
+    }
+
+    @Test
+    @DisplayName("기물의 색과 종류를 인자로 받아 해당 기물의 개수를 반환할 수 있어야 한다")
+    public void getPieceCountWithColorAndType() {
+        board.initialize();
+        assertEquals(8, board.pieceCount(Color.WHITE, Type.PAWN));
+        assertEquals(2, board.pieceCount(Color.BLACK, Type.ROOK));
+        assertEquals(1, board.pieceCount(Color.BLACK, Type.QUEEN));
     }
 }
