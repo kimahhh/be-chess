4c445dd5a0c27962f39cb1b0efb33f87522428d3
kimahhh <kimahyunn132@gmail.com>
2023-07-07 10:53:30 +0900

    Refactor: Type의 defaultPoint 수정

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -9,8 +9,8 @@ public class Piece {
         PAWN('p', 1.0),
         ROOK('r', 5.0),
         KNIGHT('n', 2.5),
-        BISHOP('b', 3),
-        QUEEN('q', 9),
+        BISHOP('b', 3.0),
+        QUEEN('q', 9.0),
         KING('k', 0.0),
         NO_PIECE('.', 0.0);
 
